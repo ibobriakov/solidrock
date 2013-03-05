b8cab3fb6f619615d2335fa997cbb2c21b4a26b6
Ilya Beda <ir4y.ix@gmail.com>
2013-03-05 16:25:30 +0800

    Add api for cover_letter

--- solidrock/urls.py
+++ solidrock/urls.py
@@ -3,12 +3,18 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from tastypie.api import Api
+from cover_letter.api import CoverLetterItemResource
+
+v1_api = Api(api_name='v1')
+v1_api.register(CoverLetterItemResource())
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(v1_api.urls)),
     url(r'^', include('main.urls')),
 )
 
