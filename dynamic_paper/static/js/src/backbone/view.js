90c6fb7d8172bc3607cdec9ac7ada07b39291d94
jackdevil <dg@allsol.ru>
2013-03-05 19:48:26 +0800

    Backbone tree collection, template tree render

--- dynamic_paper/static/js/src/backbone/view.js
+++ dynamic_paper/static/js/src/backbone/view.js
@@ -1,12 +1,29 @@
 /**
  * User: jackdevil
  */
-var PaperView = Backbone.View.extend({
-    collection: new PaperCollection(),
-    id: 'paper',
-    render: function(){
-        $(this.el).html();
-    }
-});
+var PaperView;
 
-var paper_view = new PaperView();
+$(document).ready(function () {
+    PaperView = Backbone.View.extend({
+        collection: new PaperCollection(),
+        id: 'paper',
+        template: _.template($('#papers_template').html()),
+        papers_to_json: function(collection) {
+            var that = this, json=[];
+            collection.each(function(model) {
+                var children = model.get('children');
+                model.unset('children');
+                json.push(model.toJSON());
+                if (children) {
+                    json = json.concat(that.papers_to_json(children));
+                }
+            });
+            return json;
+        },
+        render: function(){
+            var json = this.papers_to_json(this.collection);
+            $('#paper').html(this.template({'papers':json}));
+            return this;
+        }
+    });
+});
