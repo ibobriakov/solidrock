c161c93487ca17f203eaa7146c0546cff4e6cfcb
Ilya Beda <ir4y.ix@gmail.com>
2013-03-06 15:14:44 +0800

    Add parent filter

--- dynamic_paper/api.py
+++ dynamic_paper/api.py
@@ -27,5 +27,18 @@ class PaperItemResource(ModelResource):
     def dehydrate_paper(self, bundle):
         return bundle.obj.paper_id
 
+    def build_filters(self, filters=None):
+        if filters is None:
+            filters = {}
+
+        parent = filters.pop('parent', None)
+
+        orm_filters = super(PaperItemResource, self).build_filters(filters)
+
+        if parent:
+            orm_filters["parent__in"] = [int(pk) for pk in parent]
+
+        return orm_filters
+
     class Meta:
         excludes = ['level', 'lft', 'rght', 'tree_id']
