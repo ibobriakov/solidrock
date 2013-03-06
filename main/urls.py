a0708e6bc5bcc13201e5e156cc2b2ce54a50bd19
jackdevil <dg@allsol.ru>
2013-03-06 11:51:28 +0800

    Base function url resolver

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.defaults import patterns, include, url
 from main.views import MainView
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', MainView.as_view(), name='main'),
+    url(r'url-resolver.js', 'main.views.url_resolver', name='url-resolver')
 )
--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 
 class MainView(TemplateView):
     template_name = 'base.html'
+
+
+def url_resolver(request):
+    return HttpResponse('',mimetype='text/javascript')
