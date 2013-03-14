29cc3962f52a621b48a8052910f148335c3a1b54
jackdevil <dg@allsol.ru>
2013-03-14 20:39:12 +0800

    Send form data to rest backend

--- registration_rest_backend/static/js/src/backbone/models.js
+++ registration_rest_backend/static/js/src/backbone/models.js
@@ -2,14 +2,16 @@
  * Create user: jackdevil
  */
 
-function form_model_fabric(type) {
-    return Backbone.Model.extend({
+function form_model_fabric(type,attributes) {
+    var Model = Backbone.Model.extend({
         urlRoot: url_resolver[type],
+        types: attributes.types,
+        titles: attributes.titles,
         initialize: function() {
-            this.set('name',type+'#'+this.get('name'));
             this.bind('change',function(){
                 console.log(this);
             })
         }
     });
+    return new Model(attributes.fields);
 }
--- registration_rest_backend/static/js/src/backbone/views.js
+++ registration_rest_backend/static/js/src/backbone/views.js
@@ -2,30 +2,28 @@
  * Create user: jackdevil
  */
 
-function form_view_fabric(type,properties) {
+function form_view_fabric(type,attributes) {
     return Backbone.View.extend ({
+        model: form_model_fabric(type,attributes),
         template: _.template($("#form-template").html()),
-        collection: new (form_collection_fabric(type))(properties),
         render: function() {
-            var view = this;
-            this.$el.html(this.template({'fields':this.collection,'type':type}));
-            this.collection.each(function(model){
-                var object = {};
-                object[model.get('name')]=model;
-                model.rivets = rivets.bind( view.el, object );
-            });
+            this.$el.html(this.template({
+                'fields':this.model.toJSON(),
+                'model':this.model,
+                'type':type
+            }));
+            var object = {};
+            object[type] = this.model;
+            this.rivets = rivets.bind(this.el, object);
+            return this;
         }
     });
 }
 
-var job_seeker_class = form_view_fabric('registration',[
-    {'name':'first_name', 'type':'text', 'label':'First Name'},
-    {'name':'last_name', 'type':'text', 'label':'Last Name'},
-    {'name':'email', 'type':'text', 'label':'Email'},
-    {'name':'password', 'type':'password', 'label':'Password'},
-    {'name':'re-password', 'type':'password', 'label':'Re-password'},
-]);
-
-var job_seeker_view = new job_seeker_class({el:$('#registration-form')});
+var job_seeker_view = new (form_view_fabric('registration',{
+    'fields':{'first_name':'','last_name':'','email_address':'','password':'','re_password':'','user_type':'job_seeker'},
+    'types':{'first_name':'text','last_name':'text','email_address':'text','password':'password','re_password':'password','user_type':'hidden'},
+    'titles':{'first_name':'First Name','last_name':'Last Name','email_address':'Email','password':'Password','re_password':'Re-Password','user_type':''}
+}))({el:$('#registration-form')});
 
 job_seeker_view.render();
