4676dfa1e2552035dc2a789b00a1f64494e711e1
Ilya Beda <ir4y.ix@gmail.com>
2013-03-14 11:28:39 +0800

    Add JobSeekerCurrentEmployment model

--- userprofile/models/models.py
+++ userprofile/models/models.py
@@ -53,7 +53,7 @@ class JobSeeker(models.Model):
 
 class JobSeekerInformation(AddressMixin, models.Model):
     user = models.OneToOneField('userprofile.Employer')
-    photo = models.ImageField(verbose_name=_("Profile picture"), blank=True, null=True)
+    photo = models.ImageField(verbose_name=_("Profile picture(optional)"), blank=True, null=True)
     home_phone = PhoneField(verbose_name=_("Home Phone Number"), blank=True)
     daytime_phone = PhoneField(verbose_name=_("Daytime Phone Number"), blank=True)
     mobile_phone = PhoneField(verbose_name=_("Mobile Phone Number"), blank=True)
@@ -66,6 +66,21 @@ class JobSeekerInformation(AddressMixin, models.Model):
                                     default=False)
     is_driver = models.BooleanField(verbose_name=_("Do you have a current driver's licence?"),
                                     default=False)
+
+    class Meta:
+        app_label = 'userprofile'
+
+
+class JobSeekerCurrentEmployment(AddressMixin, models.Model):
+    user = models.OneToOneField('userprofile.Employer')
+    name = models.CharField(verbose_name="Name of Employer", max_length=255, blank=True)
+    position_title = models.CharField(verbose_name="Position Title", max_length=255, blank=True)
+    date_commenced = models.DateField(verbose_name="Date Commenced", blank=True)
+    department = models.CharField(verbose_name="Department/Section", max_length=255, blank=True)
+    brief = models.TextField(verbose_name="Brief Description of Duties", blank=True)
+    job_type = models.CharField(verbose_name="Fulltime, Parttime or Casual", max_length=255, blank=True)
+    last_day_of_service = models.CharField(verbose_name="Last Day of Service", max_length=255, blank=True)
+    leaving_reason = models.TextField(verbose_name="Reson for Leaving", blank=True)
 
     class Meta:
         app_label = 'userprofile'
