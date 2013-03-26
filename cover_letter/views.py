b3cbd1301b5f89b41561a5c7e6ba6725d1c5794c
jackdevil <dg@allsol.ru>
2013-03-26 18:22:51 +0800

    login required cover letter and resume

--- cover_letter/views.py
+++ cover_letter/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import redirect, get_object_or_404
+from django.utils.decorators import method_decorator
 from django.views.generic import DetailView
 from models import CoverLetter
 
-
+@login_required(login_url='/#login')
 def create_cover_letter_view(request):
     new_cover_letter = CoverLetter.objects.create(owner=request.user, name="Cover Letter")
     return redirect('cover_letter.edit', new_cover_letter.pk)
@@ -20,7 +22,12 @@ class CoverLetterView(DetailView):
             raise Http404
         return  object
 
+    @method_decorator(login_required(login_url='/#login'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(CoverLetterView, self).dispatch(request, *args, **kwargs)
 
+
+@login_required(login_url='/#login')
 def delete_cover_letter_view(request, resume_pk):
     get_object_or_404(CoverLetter, pk=resume_pk, owner=request.user).delete()
     return redirect('job_seeker.profile.base')
--- resume/views.py
+++ resume/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
+from django.utils.decorators import method_decorator
 from django.views.generic import DetailView
 from models import Resume
 
-
+@login_required(login_url='/#login')
 def create_resume_view(request):
     new_resume = Resume.objects.create(owner=request.user, name="New Resume")
     return redirect('resume.edit', new_resume.pk)
@@ -19,7 +21,12 @@ class ResumeView(DetailView):
             raise Http404
         return  object
 
+    @method_decorator(login_required(login_url='/#login'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(ResumeView, self).dispatch(request, *args, **kwargs)
 
+
+@login_required(login_url='/#login')
 def delete_resume_view(request, resume_pk):
     get_object_or_404(Resume, pk=resume_pk, owner=request.user).delete()
     return redirect('job_seeker.profile.base')
