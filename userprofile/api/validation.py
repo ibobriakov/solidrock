1cd85e212f4f1e936f8317a7700d9293cc989c3a
Ilya Beda <ir4y.ix@gmail.com>
2013-03-26 17:12:03 +0800

    Fix Employer information Form validator

--- userprofile/api/validation.py
+++ userprofile/api/validation.py
@@ -6,6 +6,6 @@ __author__ = 'ir4y'
 class EmployerResourceValidation(FormValidation):
     def is_valid(self, bundle, request=None):
         errors = super(EmployerResourceValidation, self).is_valid(bundle, request)
-        if 'agree' not in bundle.data or not bundle.data['agree']:
+        if 'agree' not in bundle.data or bundle.data['agree'] != 'true':
             errors['agree'] = ['You should accept user agreement']
         return errors
