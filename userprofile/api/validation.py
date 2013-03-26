5d16b0f6997a42f3ae4c86ea88f1040e0ca23756
Ilya Beda <ir4y.ix@gmail.com>
2013-03-26 15:47:09 +0800

    Error output fix

--- userprofile/api/validation.py
+++ userprofile/api/validation.py
@@ -7,5 +7,5 @@ class EmployerResourceValidation(FormValidation):
     def is_valid(self, bundle, request=None):
         errors = super(EmployerResourceValidation, self).is_valid(bundle, request)
         if 'agree' not in bundle.data or not bundle.data['agree']:
-            errors['agree'] = 'You should accept user agreement'
+            errors['agree'] = ['You should accept user agreement']
         return errors
