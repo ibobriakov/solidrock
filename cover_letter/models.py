04c4533bb8eb7dd58af8c70d8131b5c7b6eb095b
jackdevil <dg@allsol.ru>
2013-03-27 19:20:46 +0800

    Cover letter group right block

--- cover_letter/models.py
+++ cover_letter/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.utils.translation import ugettext_lazy as _
 from dynamic_paper.models import paper_item_factory
-from dynamic_paper.models.signals import get_signal_for_page
+from dynamic_paper.models.signals import get_signal_for_page, get_signal_for_page_item
 from dynamic_paper.utils import get_paper_item
 
 
@@ -27,15 +27,8 @@ class  CoverLetterItem(paper_item_factory('cover_letter.CoverLetter', verbose_na
 
 
 cover_letter_template = [
-    lambda paper:CoverLetterItem(type=get_paper_item('text'), item_class="paper-right", value="Name", paper=paper),
-
-    lambda paper:CoverLetterItem(type=get_paper_item('text'), item_class="paper-right", value="Address", paper=paper),
-
-    lambda paper:CoverLetterItem(type=get_paper_item('text'), item_class="paper-right", value="Phone Number",
-                                 paper=paper),
-
-    lambda paper:CoverLetterItem(type=get_paper_item('text'), item_class="paper-right", value="Email Address",
-                                 paper=paper),
+    lambda paper:CoverLetterItem(type=get_paper_item('right_list'), item_class="paper-right paper-block",
+                                 value=" ", paper=paper),
 
     lambda paper:CoverLetterItem(type=get_paper_item('text'), item_class="title mini date", value="Date", paper=paper),
 
@@ -64,8 +57,29 @@ cover_letter_template = [
     lambda paper:CoverLetterItem(type=get_paper_item('text'), item_class="paper-block",
                                  value="Quick-reference contact number", paper=paper), ]
 
+cover_letter_type_template = {
+    'right': [
+        lambda cover_letter_item:CoverLetterItem(type=get_paper_item('text'), item_class="paper-right", value="Name",
+                                                 paper=cover_letter_item.paper, parent=cover_letter_item),
+
+        lambda cover_letter_item:CoverLetterItem(type=get_paper_item('text'), item_class="paper-right", value="Address",
+                                                 paper=cover_letter_item.paper, parent=cover_letter_item),
+
+        lambda cover_letter_item:CoverLetterItem(type=get_paper_item('text'), item_class="paper-right", value="Phone Number",
+                                                 paper=cover_letter_item.paper, parent=cover_letter_item),
+
+        lambda cover_letter_item:CoverLetterItem(type=get_paper_item('text'), item_class="paper-right", value="Email Address",
+                                                 paper=cover_letter_item.paper, parent=cover_letter_item),
+    ]
+}
+
 
 def cover_letter_proxy(instance, created, **kwargs):
     return get_signal_for_page(cover_letter_template)(instance, created, **kwargs)
 
+
+def cover_letter_item_proxy(instance, created, **kwargs):
+    return get_signal_for_page_item(cover_letter_type_template)(instance, created, **kwargs)
+
 post_save.connect(cover_letter_proxy, sender=CoverLetter)
+post_save.connect(cover_letter_item_proxy, sender=CoverLetterItem)
