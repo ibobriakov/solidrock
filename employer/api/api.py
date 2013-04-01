69ed7bd99ad7c127b66cc6f8c4cd56fd60a9c513
Ilya Beda <ir4y.ix@gmail.com>
2013-04-01 14:24:31 +0800

    Add document_type_name to JobUploadDocumentResource

--- employer/api/api.py
+++ employer/api/api.py
@@ -132,6 +132,10 @@ class JobUploadDocumentTypeResource(ModelResource):
 class JobUploadDocumentResource(ModelResource):
     job = fields.ToOneField('employer.api.JobResource', 'job')
     document_type = fields.ToOneField('employer.api.JobUploadDocumentTypeResource', 'document_type', full=True)
+    document_type_name = fields.CharField(readonly=True)
+
+    def dehydrate_document_type_name(self, bundle):
+        return bundle.obj.document_type.__unicode__()
 
     class Meta:
         queryset = JobUploadDocument.objects.all()
