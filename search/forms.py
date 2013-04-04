badd2bde4c52f441d3ad17c40943dc3524911d56
Ilya Beda <ir4y.ix@gmail.com>
2013-04-04 14:38:42 +0800

    Search form refactor Add Django-Select2, abd use it in SearchForm. Some style refactor

--- search/forms.py
+++ search/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django_select2 import Select2Widget
 from employer.models import JobCategory, JobSubCategory, JobLocation, JobArea
 
 __author__ = 'ir4y'
@@ -7,16 +8,22 @@ __author__ = 'ir4y'
 class SearchForm(forms.Form):
     keywords = forms.CharField(required=False)
     executive_positions = forms.ChoiceField(required=False,
-                                            choices=((0, "All Executive Positions"),
-                                                     (1, "All Executive Positions")))
+                                            choices=(('', ''),  # empty option for select 2
+                                                     (0, "All Executive Positions"),
+                                                     (1, "All Executive Positions")),
+                                            widget=Select2Widget(select2_options={'placeholder': 'All Positions'}))
     categories = forms.ModelChoiceField(queryset=JobCategory.objects.none(),
-                                        required=False, empty_label='Any Category')
+                                        required=False, empty_label='',
+                                        widget=Select2Widget(select2_options={'placeholder': 'Any Category'}))
     location = forms.ModelChoiceField(queryset=JobLocation.objects.none(),
-                                      required=False, empty_label='All Australia')
+                                      required=False, empty_label='',
+                                      widget=Select2Widget(select2_options={'placeholder': 'All Australia'}))
     sub_categories = forms.ModelChoiceField(queryset=JobSubCategory.objects.none(),
-                                            required=False, empty_label='Any Sub-Category')
+                                            required=False, empty_label='',
+                                            widget=Select2Widget(select2_options={'placeholder': 'All'}))
     area = forms.ModelChoiceField(queryset=JobArea.objects.none(),
-                                  required=False, empty_label='All')
+                                  required=False, empty_label='',
+                                  widget=Select2Widget(select2_options={'placeholder': 'All'}))
 
     def __init__(self, *args, **kwargs):
         super(SearchForm, self).__init__(*args, **kwargs)
