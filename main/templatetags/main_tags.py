5ba3f3fdcadb0f0c771eef7f50fa18f0c0e69bd3
Ilya Beda <ir4y.ix@gmail.com>
2013-04-05 16:49:02 +0800

    Add add_class filter

--- main/templatetags/main_tags.py
+++ main/templatetags/main_tags.py
@@ -1,4 +1,6 @@
+import re
 from django import template
+from django.utils.safestring import mark_safe
 
 __author__ = 'ir4y'
 
@@ -19,3 +21,20 @@ def is_job_seeker(user):
 @register.filter()
 def is_employer(user):
     return user.user_type
+
+class_re = re.compile(r'(?<=class=["\'])(.*)(?=["\'])')
+
+@register.filter
+def add_class(value, css_class):
+    string = unicode(value)
+    match = class_re.search(string)
+    if match:
+        m = re.search(r'^%s$|^%s\s|\s%s\s|\s%s$' % (css_class, css_class,
+                                                    css_class, css_class), match.group(1))
+        print match.group(1)
+        if not m:
+            return mark_safe(class_re.sub(match.group(1) + " " + css_class,
+                                          string))
+    else:
+        return mark_safe(string.replace('>', ' class="%s">' % css_class))
+    return value
