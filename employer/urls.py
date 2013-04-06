f21c1bb5c209c6d7c8e4296ed6ab31f02e3fde4e
Ilya Beda <ir4y.ix@gmail.com>
2013-04-06 15:30:10 +0800

    Add dummy employer view employer view refactor

--- employer/urls.py
+++ employer/urls.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
 from views import EmployerView, EmployerEditView, create_job_view, JobListView,\
-    EditJobView, delete_job_view, JobPublicView
+    EditJobView, delete_job_view, JobPublicView, EmployerPublicView
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', EmployerView.as_view(), name='employer.profile.base'),
+    url(r'^(?P<pk>\d+)/$', EmployerPublicView.as_view(), name='employer.profile.public'),
     url(r'^edit/$', EmployerEditView.as_view(), name='employer.profile.edit'),
     url(r'^job/create$', create_job_view, name='employer.job.create'),
     url(r'^job/list', JobListView.as_view(), name='employer.job.list'),
