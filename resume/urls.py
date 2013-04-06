a327cb6de5594233cad9d1bafd54aa805f6a5950
Ilya Beda <ir4y.ix@gmail.com>
2013-04-06 14:56:34 +0800

    Add dummy resume view

--- resume/urls.py
+++ resume/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
-from views import create_resume_view, ResumeView, delete_resume_view
+from views import create_resume_view, ResumeView, ResumePublicView, delete_resume_view
 
 
 __author__ = 'ir4y'
@@ -9,5 +9,6 @@ urlpatterns = patterns(
     '',
     url(r'^create/$', create_resume_view, name='resume.create'),
     url(r'^(?P<pk>\d+)/$', ResumeView.as_view(), name='resume.edit'),
+    url(r'^(?P<pk>\d+)/view$', ResumePublicView.as_view(), name='resume.view'),
     url(r'^(?P<resume_pk>\d+)/delete$', delete_resume_view, name='resume.delete'),
 )
