a71b341e582ce47f8a3c988f4cdc232806ba76b5
Ilya Beda <ir4y.ix@gmail.com>
2013-04-08 21:59:46 +0800

    Template and url fixes

--- employer/urls.py
+++ employer/urls.py
@@ -6,12 +6,5 @@ from views import EmployerView, EmployerEditView, create_job_view, JobListView,\
 
 urlpatterns = patterns(
     '',
-    url(r'^$', EmployerView.as_view(), name='employer.profile.base'),
-    url(r'^(?P<pk>\d+)/$', EmployerPublicView.as_view(), name='employer.profile.public'),
-    url(r'^edit/$', EmployerEditView.as_view(), name='employer.profile.edit'),
-    url(r'^job/create$', create_job_view, name='employer.job.create'),
-    url(r'^job/list', JobListView.as_view(), name='employer.job.list'),
-    url(r'^job/(?P<pk>\d+)/edit$', EditJobView.as_view(), name='employer.job.edit'),
-    url(r'^job/(?P<pk>\d+)/view', JobPublicView.as_view(), name='employer.job.view'),
-    url(r'^job/(?P<pk>\d+)/delete$', delete_job_view, name='employer.job.delete'),
+    url(r'^$', EmployerEditView.as_view(), name='employer.profile.base'),
 )
--- promo/urls.py
+++ promo/urls.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 from views import EmployersPromo
 
 
 urlpatterns = patterns(
     '',
     url(r'^employers/$', EmployersPromo.as_view(), name='promo.employers'),
+    url(r'^coming_soon/$', TemplateView.as_view(template_name="landing/comming_soon.html"), name='promo.coming_soon'),
 )
