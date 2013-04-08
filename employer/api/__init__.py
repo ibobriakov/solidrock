962ab4cd8a976c307edf9e715b62bdbd4c789fae
Ilya Beda <ir4y.ix@gmail.com>
2013-04-08 11:58:26 +0800

    Add JobArea resource

--- employer/api/__init__.py
+++ employer/api/__init__.py
@@ -1,7 +1,7 @@
 from api import *
 
 __author__ = 'ir4y'
-__all__ = ['JobResource', 'LocationResource', 'HourResource',
+__all__ = ['JobResource', 'LocationResource', 'JobAreaResource', 'HourResource',
            'EmploymentTypeResource', 'SpecialConditionResource', 'EssentialResource', 'DesireableResource',
            'JobCategoryResource', 'JobSubCategoryResource', 'JobUploadDocumentResource',
            'JobSelectedCategoryResource', 'JobSelectedSubCategoryResource', 'JobExecutivePositionsResource', ]
