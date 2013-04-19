d83a6ba9eeb6226661705f344c8f712c2cb56335
Ilya Beda <ir4y.ix@gmail.com>
2013-04-19 09:22:58 +0800

    Add tastypie_rivets app

---
+++ tastypie_rivets/forms/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.forms.models import ModelChoiceField
+from tastypie_rivets.forms.fields import ResourceSelect
+
+__author__ = 'ir4y'
+
+
+def rivet_modelform_factory(base_name):
+    class RivetModelForm(forms.ModelForm):
+        def __init__(self, *args, **kwargs):
+            super(RivetModelForm, self).__init__(*args, **kwargs)
+            for name, field in self.fields.iteritems():
+                field.widget.attrs['data-value'] = base_name + "." + name
+                setattr(field, 'type', base_name)
+                if isinstance(field, ModelChoiceField):
+                    required = field.required | (name in getattr(self.Meta.model, 'REQUIRED_FIELDS', []))
+                    # REQUIRED_FIELDS dog-nail see employer/models.py:54: %-)
+                    kwargs = {'resource_name': field.queryset.model.__name__.lower(),
+                              'queryset': field.queryset,
+                              'empty_label': '',
+                              'required': required}
+                    widgets = getattr(self.Meta, 'widgets', {})
+                    if name in widgets:
+                        kwargs['widget'] = widgets[name]
+                    self.fields[name] = ResourceSelect(**kwargs)
+    return RivetModelForm
