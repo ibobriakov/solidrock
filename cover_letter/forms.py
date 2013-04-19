ee2222feb1288817ee942dd7fd172a9936e52bd7
Ilya Beda <ir4y.ix@gmail.com>
2013-04-19 09:39:57 +0800

    Migrate to tastypie_rivets form fabrics

--- cover_letter/forms.py
+++ cover_letter/forms.py
@@ -1,12 +1,17 @@
 from django import forms
+from django.core.urlresolvers import reverse
 from django_select2 import Select2Widget
-from dynamic_paper.forms import url_choice_field_fabric
 from models import CoverLetter
+from tastypie_rivets.forms.model import url_choice_field_fabric
 
 __author__ = 'ir4y'
 
 
+def get_url_fun(obj):
+    return reverse('cover_letter.edit', args=[obj.pk])
+
+
 class CoverLetterSelectForm(forms.Form):
-    cover_letter = url_choice_field_fabric('cover_letter.edit')\
+    cover_letter = url_choice_field_fabric(get_url_fun)\
             (queryset=CoverLetter.objects.none(),required=True, empty_label="",
              widget=Select2Widget(select2_options={'placeholder': 'Jump to Cover Letter'}))
--- resume/forms.py
+++ resume/forms.py
@@ -1,12 +1,17 @@
 from django import forms
+from django.core.urlresolvers import reverse
 from django_select2 import Select2Widget
-from dynamic_paper.forms import url_choice_field_fabric
 from models import Resume
+from tastypie_rivets.forms.model import url_choice_field_fabric
 
 __author__ = 'ir4y'
 
 
+def get_url_fun(obj):
+    return reverse('resume.edit', args=[obj.pk])
+
+
 class ResumeSelectForm(forms.Form):
-    resume = url_choice_field_fabric('resume.edit')\
+    resume = url_choice_field_fabric(get_url_fun)\
             (queryset=Resume.objects.none(),required=True, empty_label="",
              widget=Select2Widget(select2_options={'placeholder': 'Jump to Resume'}))
