640ad18589d5f0db2979e709968b342e2d074bd4
Ilya Beda <ir4y.ix@gmail.com>
2013-05-11 14:57:42 +0800

    get_resource_class refactor

--- userprofile/templatetags/utils.py
+++ userprofile/templatetags/utils.py
@@ -1,26 +1,18 @@
-from tastypie.resources import ModelResource
-from cover_letter import api as cover_letter_api
-from registration_rest_backend import api as registration_rest_backend_api
-from resume import api as resume_api
-from userprofile import api as userprofile_api
-from employer import api as employer_api
-from payment import api as payment_api
+from django.conf import settings
+from django.utils.importlib import import_module
+
 
 __author__ = 'ir4y'
 
 
 def get_resource_class(model_class):
-    api_list = []
-
-    def get_import_list(module):
-        modules = []
-        for item in module.__all__:
-            resource = getattr(module, item)()
-            if isinstance(resource, ModelResource):
-                modules.append(resource)
-        return modules
-
-    for item in map(get_import_list, [cover_letter_api, registration_rest_backend_api, resume_api,
-                                      userprofile_api, employer_api, payment_api]):
-        api_list.extend(item)
-    return {resource._meta.queryset.model: resource for resource in api_list}[model_class]
+    for app in settings.INSTALLED_APPS:
+        try:
+            resource_api = import_module('%s.api' % app)
+            for resource_klass_name in resource_api.__all__:
+                resource_klass = getattr(resource_api, resource_klass_name)()
+                if resource_klass._meta.queryset.model == model_class:
+                    return resource_klass
+        except:
+            continue
+    return None
