fb3434cd62abf1801ac24b0460bc0172406bb565
jackdevil <dg@allsol.ru>
2013-06-10 09:11:06 +0800

    Dynamic paper view, select location href

--- dynamic_paper/static/js/src/angular/controllers.js
+++ dynamic_paper/static/js/src/angular/controllers.js
@@ -4,12 +4,20 @@
 
 var dpc = {}; // Dynamic Paper Controllers Dict
 
-dpc.DynamicPaperCtrl = function($scope) {
+dpc.DynamicPaperCtrl = function ($scope) {
     $scope.paper = {};
-    $scope.set_data = function(data){
+    $scope.set_data = function (data) {
         $scope.paper.data = data.data;
         $scope.paper.name = data.name[0];
     };
+
+    $('#id_resume').change(function () {
+        window.location.replace(this.value);
+    });
+
+    $('#id_cover_letter').change(function () {
+        window.location.replace(this.value);
+    });
 };
 
 DynamicPaperApp.controller(dpc);
