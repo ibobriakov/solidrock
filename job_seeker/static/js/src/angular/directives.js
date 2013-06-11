fe54512d010b4cd32289cbf538d41c981b769065
jackdevil <dg@allsol.ru>
2013-06-11 15:23:43 +0800

    Upload photo

--- job_seeker/static/js/src/angular/directives.js
+++ job_seeker/static/js/src/angular/directives.js
@@ -7,6 +7,39 @@
  */
 var directives = {};
 
+directives.upload = function () {
+    return {
+        restrict: 'E',
+        scope: {
+            uploadUrl: '@',
+            removeUrl: '@',
+            photo: '='
+        },
+        template: '<div>Profile Picture (optional)</div>\n<div class="img">\n    <img id="avatar_img" src="{[{ photo }]}">\n\n    <div class="links">\n        <a id="link_upload_file" href="">Upload Photo</a>|\n        <a id="link_remove_file" url="" href="">Delete Picture</a>\n    </div>\n</div>\n<input class="_hide" id="fileupload" type="file" name="files[]" data-url="/upload/job_seeker_photo/">',
+        link: function (scope, element, attr) {
+            element.find('#fileupload').fileupload({
+                singleFileUploads: true,
+                dataType: 'json',
+                done: function (e, data) {
+                    element.find('#avatar_img').attr('src', data.result.url);
+                }
+            });
+            element.find('#link_upload_file').on('click', function () {
+                element.find('#fileupload').trigger('click');
+                return false;
+            });
+            element.find('#link_remove_file').on('click', function () {
+                $.ajax({
+                    url: '/remove/job_seeker_photo/'
+                }).done(function (data) {
+                        $('#avatar_img').attr('src', data.url);
+                    });
+                return false;
+            })
+        }
+    }
+};
+
 directives.activated = function () {
     return {
         restrict: "A",
