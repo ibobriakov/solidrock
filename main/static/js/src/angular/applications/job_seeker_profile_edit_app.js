496ba0c5040d3bea3181738e1ab9ac47a2b15806
jackdevil <dg@allsol.ru>
2013-06-12 13:24:52 +0800

    js header fixture

--- main/static/js/src/angular/applications/job_seeker_profile_edit_app.js
+++ main/static/js/src/angular/applications/job_seeker_profile_edit_app.js
@@ -1,9 +1,5 @@
 /**
- * Created with PyCharm.
  * User: jackdevil
- * Date: 10.06.13
- * Time: 10:14
- * To change this template use File | Settings | File Templates.
  */
 
 var AplInfoApp = angular.module('AplInfoApp', ['ui']);
--- main/static/js/src/angular/controllers/employer_job_apply_ctrl.js
+++ main/static/js/src/angular/controllers/employer_job_apply_ctrl.js
@@ -1,9 +1,5 @@
 /**
- * Created with PyCharm.
  * User: jackdevil
- * Date: 11.05.13
- * Time: 12:24
- * To change this template use File | Settings | File Templates.
  */
 
 ApplyJobApp.controller('ApplyJobCtrl',function($scope, $http) {
