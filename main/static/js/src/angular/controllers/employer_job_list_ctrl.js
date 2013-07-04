60b29207639e086c981d50266c9dfba6abfad621
jackdevil <dg@allsol.ru>
2013-07-04 16:09:39 +0800

    Archived job

--- main/static/js/src/angular/controllers/employer_job_list_ctrl.js
+++ main/static/js/src/angular/controllers/employer_job_list_ctrl.js
@@ -8,24 +8,34 @@ var listJobCtrl = {};
 
 listJobCtrl.ListJobCtrl = function ($scope, $http, $location) {
     $scope.url = '/api/v1/jobbanner/';
-    $scope.limit = 5;
+    $scope.limit = 9;
     $scope.offset = 0;
     $scope.current = 0;
     $scope.pages = 1;
     $scope.archived = false;
+    
     $scope.get_url = function(item){
-        if ($scope.archived) {
-            return $scope.url + '?limit=' + $scope.limit + '&offset=' + $scope.limit*item + '&archived=true';
-        } else {
-            return $scope.url + '?limit=' + $scope.limit + '&offset=' + $scope.limit*item; 
-        }
+        return $scope.url + '?limit=' + $scope.limit + '&offset=' + $scope.limit*item + '&archived=' + $scope.archived;
     };
-    $('.preloader').show();
-    $http.get($scope.url+"?limit="+$scope.limit).success(function(data, status, headers, config) {
-        $scope.data = data;
-        $scope.pages = parseInt(data.meta.total_count/$scope.limit)
-        $('.preloader').hide();
-    });
+   
+    $scope.set_archived = function(archived){
+        $scope.archived = archived;
+        $scope.get_data($scope.get_url(0),0);
+    }
+
+    $scope.get_data = function(url,page) {
+        if (!url) return;
+        $('.preloader').show();
+        $http.get(url).success(function(data, status, headers, config) {
+            $scope.data = data;
+            $scope.current = page;
+            $scope.pages = parseInt(data.meta.total_count/$scope.limit)
+            $('.preloader').hide();
+        });
+    };
+
+    $scope.get_data($scope.get_url(0),0);
+
 };
 
 MainApp.filter('makeRange', function() {
