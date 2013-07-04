9f6214e472c1219ec62907a11fc489ad2222e05d
jackdevil <dg@allsol.ru>
2013-07-04 15:05:19 +0800

    Paginate fixture

--- main/static/js/src/angular/controllers/employer_job_list_ctrl.js
+++ main/static/js/src/angular/controllers/employer_job_list_ctrl.js
@@ -7,18 +7,35 @@
 var listJobCtrl = {};
 
 listJobCtrl.ListJobCtrl = function ($scope, $http, $location) {
-    var url = '/api/v1/jobbanner/';
-    var limit = 5;
-    var offset = 0;
+    $scope.url = '/api/v1/jobbanner/';
+    $scope.limit = 5;
+    $scope.offset = 0;
     $scope.current = 0;
     $scope.pages = 1;
-
+    $scope.archived = false;
+    $scope.get_url = function(item){
+        if ($scope.archived) {
+            return $scope.url + '?limit=' + $scope.limit + '&offset=' + $scope.limit*item + '&archived=true';
+        } else {
+            return $scope.url + '?limit=' + $scope.limit + '&offset=' + $scope.limit*item; 
+        }
+    };
     $('.preloader').show();
-    $http.get(url+"?limit="+limit).success(function(data, status, headers, config) {
+    $http.get($scope.url+"?limit="+$scope.limit).success(function(data, status, headers, config) {
         $scope.data = data;
-        $scope.pages = parseInt(data.meta.total_count/limit + 1);
+        $scope.pages = parseInt(data.meta.total_count/$scope.limit)
         $('.preloader').hide();
-    })
+    });
 };
+
+MainApp.filter('makeRange', function() {
+    return function(input) {
+        var result = [];
+        for (var i = 0; i <= input; i++) {
+            result.push(i);
+        }
+        return result;
+    }
+});
 
 MainApp.controller(listJobCtrl);
